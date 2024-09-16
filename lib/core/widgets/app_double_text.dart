7a5227a68bffec5fb9d83798579f34ad015c28ea
Vlad Vasilkovsky <35891879+teraGL@users.noreply.github.com>
2024-09-16 11:30:35 +0300

    Update dart linter to allow lines_longer_than_80_chars

---
+++ lib/core/widgets/app_double_text.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppDoubleText extends StatelessWidget {
+  const AppDoubleText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,7 +10,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       // we want scrollable effect
-      backgroundColor: AppStyles.bgColor ,
+      backgroundColor: AppStyles.bgColor,
       body: ListView(
         children: [
           const SizedBox(height: 40),
@@ -33,9 +33,8 @@ class HomeScreen extends StatelessWidget {
                       width: 50,
                       height: 50,
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: const DecorationImage(image: AssetImage(AppMedia.logo))
-                      ),
+                          borderRadius: BorderRadius.circular(10),
+                          image: const DecorationImage(image: AssetImage(AppMedia.logo))),
                     )
                   ],
                 ),
@@ -43,8 +42,8 @@ class HomeScreen extends StatelessWidget {
                 Container(
                   padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
                   decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: const Color(0xFFF4F6FD),
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xFFF4F6FD),
                   ),
                   child: const Row(
                     // mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -53,7 +52,7 @@ class HomeScreen extends StatelessWidget {
                       Text("Search"),
                     ],
                   ),
-                ), 
+                ),
               ],
             ),
           )
